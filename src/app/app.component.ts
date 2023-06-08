24bf83ee45c3e68f577a08751328b163f95dc2e7
claudinei.fedora <claudineigomes8@hotmail.com>
2023-06-08 20:41:17 -0300

    Insert PWA to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'real_estate_istallment_calculator';
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled){
+      this.swUpdate.available.subscribe(() => {
+        if(confirm("You're using an old version of the control panel. Want to update?")){
+          window.location.reload();
+        }
+      })
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ValueSelectorComponent } from './value-selector/value-selector.component';
 import { ValueSelectorInstallmentsComponent } from './value-selector-installments/value-selector-installments.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,13 @@ import { ValueSelectorInstallmentsComponent } from './value-selector-installment
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
